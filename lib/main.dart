449249a9ede48b8b7252b0061c9cc2d1575598b6
shivanimurthy <hiteshsysorex@gmail.com>
2021-12-15 15:19:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LocationApp());
+  }
+}
+
+class LocationApp extends StatefulWidget {
+  const LocationApp({Key? key}) : super(key: key);
+
+  @override
+  _LocationAppState createState() => _LocationAppState();
+}
+
+class _LocationAppState extends State<LocationApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location Services'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.location_on,
+              size: 50.0,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {}, child: Text('Get current location'))
+          ],
+        ),
+      ),
+    );
+  }
+}
